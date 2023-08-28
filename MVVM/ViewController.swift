a76e71ba8da50465cf879e9f39ee14c89c16e8c1
AlexeyManankov <virtbrain@gmail.com>
2023-08-28 21:47:19 +0500

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Alexey Manankov on 28.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
